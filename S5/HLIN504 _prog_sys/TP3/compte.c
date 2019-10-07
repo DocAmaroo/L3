54c802130ee1f75f3adebd3aeea3a45b0fcfb4be
C.Thomas <thomas.canta@etu.umontpellier.fr>
2019-10-07 17:20:02 +0200

    update

--- S5/HLIN504 _prog_sys/TP3/compte.c
+++ S5/HLIN504 _prog_sys/TP3/compte.c
@@ -1,30 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
 
-void compte( size_t *buffer, char const *argv ){
-    FILE* f = fopen(argv, "r");
-    char c = fgetc(f);
-    size_t res = 0;
+void compte(char const *argv)
+{
 
-    while( c != EOF ){
-        res++;
-        c = fgetc(f);
-    }
-
-    fclose(f);
-}
-
-int main(int argc, char const *argv[]){
-    if ( argc < 2 ){
-        printf("[ ERROR ] no file name give \n");
+    int f = open(argv, 0);
+    if( f == -1 ){
+        perror("[ ERROR ] no file name give \n");
         exit(0);
     }
 
-    char buffer = malloc(128*sizeof(char));
-    compte(buffer, argv[1]);
+    char buffer[256];
+    for (int i = 0; i < 256; i++) { buffer[i] = 0; }
+
+    // on récupère le caractère
+    char c;
+
+    // Tant qu'on est pas à la fin du fichier
+    while ( read(f, &c, 1) == 1 )
+    {
+        buffer[c]++;
+    }
+
+    int total = 0;
+    printf("les différents caractères sont : ");
+
+    for (int i = 0; i < 256; i++)
+    {
+        if (buffer[i] != 0)
+        {
+            total++;
+            printf("%c:%i, ", i, buffer[i]);
+        }
+    }
+    printf("\nIl y a %i caractère différents\n", total);
+
+    close(f);
+}
+
+int main(int argc, char const *argv[])
+{
+    if (argc < 2)
+    {
+        perror("[ ERROR ] no file name give \n");
+        exit(0);
+    }
+    compte(argv[1]);
 
     return 0;
-
 }
