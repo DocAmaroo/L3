24210fabdccfd3b6615bf3b7afd78ada006aa8ab
C.Thomas <thomas.canta@etu.umontpellier.fr>
2019-10-09 23:33:53 +0200

    ceNouveauGitFranchementIlDechire

--- S5/HLIN504 _prog_sys/TP3/position/dicho.c
+++ S5/HLIN504 _prog_sys/TP3/position/dicho.c
@@ -20,23 +20,62 @@ int main(int argc , char * argv[]) {
         return 2;
     }
 
-    char* toFind = argv[2];
-    int file_length = lseek(file, 0, SEEK_END);
-    char* c;
-    int nb;
-    
+    /**
+     * @brief Déclaration des variables
+     * 
+     */
+    char toFind = argv[2][0],
+    c;
+
+    int deb = 0,
+    fin = lseek(file, 0, SEEK_END)-1,
+    mid = 0;
+
+    /**
+     * @brief Si fin = 0 => fichier vide
+     * 
+     */
+    if ( fin == 0 ){
+        printf("Votre fichier est vide !\n");
+        exit(EXIT_FAILURE);
+    }
+
     /**
      * @brief On se met au milieu du fichier
-     * Si code ASCII du caractère au centre est < a celui qu'on cherche
-     * alors on positionne notre curseur au centre pour lire la suite
-     * Sinon il entre le début et la moitié
-     */
-    lseek(file, 0, SEEK_CUR);
-    nb = read(file, &c, 1);
+     * Tant que le curseur de début est < curseur de fin et
+     * qu'on a pas trouvé notre caractère on continue la boucle
+     */ 
+    while( deb <= fin && c != toFind )
+    {
+        mid = (deb+fin)/2; // Nouveau milieu
+        lseek(file, mid, SEEK_SET); // positionne le curseur au centre
+        read(file, &c, 1); // on récupère le nouveau caractère
+        
+        /**
+         * @brief Si le caractère ASCII du fichier est < à celui qu'on recherche
+         * alors il est dans la partie entre mid et fin
+         */
+        if ( c < toFind ){
+            deb = mid+1;
+        }
+
+        /**
+         * @brief Sinon c'est entre deb et mid 
+         */
+        else{
+            fin = mid-1;
+        }
+        printf("END : deb = %i | fin = %i | mid = %i\n", deb,fin,mid);
+        
+    }
+
     printf("c = %c", c);
-
-
+    if ( c == toFind ){
+        printf("Votre caractère a été trouvé en position %i\n", mid);
+    }
+    else{
+        printf("Votre caractère est introuvable !\n");
+    }
+    close(file);
     return 0;
 }
-
-// https://www.geeksforgeeks.org/lseek-in-c-to-read-the-alternate-nth-byte-and-write-it-in-another-file/
