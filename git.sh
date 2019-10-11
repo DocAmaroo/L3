6cf2168b85ddb8c993f69e79104ab23b4b45b779
C.Thomas <thomas.canta@etu.umontpellier.fr>
2019-10-11 14:08:03 +0200

    [A[B[B

--- git.sh
+++ git.sh
@@ -7,7 +7,7 @@ NC='\033[0m' # No Color
 
 git add *
 
-printf "${ORANGE}\n## REMOVING FILES FROM "add *" ## \n#################################\n ${NC}"
+printf "${ORANGE}\n## REMOVING FILES FROM 'add *' ## \n#################################\n${NC}"
 git rm --cached S5/HLIN501_algo_de_graphes/cours.pdf
 
 printf "${ORANGE}\n## STATUS ##\n############\n${NC}"
