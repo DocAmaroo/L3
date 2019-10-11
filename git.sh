08d5daefe1ede20e06545668f189528639c4388b
C.Thomas <thomas.canta@etu.umontpellier.fr>
2019-10-11 14:03:49 +0200

    update

--- git.sh
+++ git.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+
+# CONSTANT COLOR
+RED='\033[0;31m'
+ORANGE='\033[0;33'
+NC='\033[0m' # No Color
+
 git add *
 
-printf '\n## REMOVING FILES FROM "add *" ## \n'
-printf '#################################\n'
+printf '${ORANGE}\n## REMOVING FILES FROM "add *" ## \n'
+printf '#################################\n${NC}'
 git rm --cached S5/HLIN501_algo_de_graphes/cours.pdf
 
-printf '\n## STATUS ##\n'
-printf '############\n'
+printf '${ORANGE}\n## STATUS ##\n'
+printf '############\n${NC}'
 git status
 
-printf '## COMMIT MESSAGE ##\n'
-printf '####################\n' 
+printf '${ORANGE}## COMMIT MESSAGE ##\n'
+printf '####################\n${NC}' 
 read commitMessage
 
 git commit -am "$commitMessage"
