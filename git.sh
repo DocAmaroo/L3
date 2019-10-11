b54b4319542a2a31f685cd9724642b3dbececc17
C.Thomas <thomas.canta@etu.umontpellier.fr>
2019-10-11 14:05:13 +0200

    marchepas

--- git.sh
+++ git.sh
@@ -7,16 +7,13 @@ NC='\033[0m' # No Color
 
 git add *
 
-printf '${ORANGE}\n## REMOVING FILES FROM "add *" ## \n'
-printf '#################################\n${NC}'
+printf '${ORANGE}\n## REMOVING FILES FROM "add *" ## \n#################################\n ${NC}'
 git rm --cached S5/HLIN501_algo_de_graphes/cours.pdf
 
-printf '${ORANGE}\n## STATUS ##\n'
-printf '############\n${NC}'
+printf '${ORANGE}\n## STATUS ##\n############\n${NC}'
 git status
 
-printf '${ORANGE}## COMMIT MESSAGE ##\n'
-printf '####################\n${NC}' 
+printf '${ORANGE}## COMMIT MESSAGE ##\n####################\n${NC}'
 read commitMessage
 
 git commit -am "$commitMessage"
