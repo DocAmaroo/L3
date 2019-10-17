1754b741639dadcb9653322290bdd0dc822381b3
C.Thomas <thomas.canta@etu.umontpellier.fr>
2019-10-17 22:25:12 +0200

    update

--- S5/HLIN510_web/Javascript/TP1/script.js
+++ S5/HLIN510_web/Javascript/TP1/script.js
@@ -1,39 +1,64 @@
-function test( noeud ){
+window.onload=function(){explorer();}
+
+function test( noeud, dest, indent ){
+
+    if ( noeud == null || noeud == dest ){ return; }
     
-    var type = "null";
+    var param = {type : "", color : "", text : ""};
+    
     switch(noeud.nodeType){
         case 1:
-            console.log("je suis un element");
-            type = "element";
+            param.type = "Element";
+            param.color = "#0084FF";
             break;
         case 2:
-            console.log("je suis un attribut");
-            type = "attribut";
+            param.type = "Attribut";
+            param.color = "#FF8100";
             break;
         case 3:
-            console.log("je suis un texte");
-            type = "texte";
+            param.type = "Text";
+            param.color = "#0CE8C0";
+            param.text = noeud.nodeValue;
             break;
         default : 
-            console.log("je suis autre chose")
-            type = "null";
+            param.type = "Document";
+            param.color = "#FFEC00";
     }
 
-    console.log(noeud.childNodes);
-}
+    /**
+     * On crée une nouvelle division
+     */
+    var new_div = document.createElement("div");
 
+    /**
+     * On ajoute le texte et la couleur de bg dans le div que l'on vient de créer 
+     * en indentant au passage avec des "__"
+     */
+    var textNode = document.createTextNode('');
+    for (let i = 0; i < indent; i++) { textNode.appendData("__"); }
+    textNode.appendData(param.type + " - " + noeud.nodeName + param.text);
+    new_div.appendChild(textNode);
+    new_div.style.cssText = "background:"+param.color;
+
+    /**
+     * On l'affiche
+     */
+    dest.appendChild(new_div);
+
+    /**
+     * Pour chaque enfant on applique la fonction
+     */
+    var child = noeud.childNodes;
+    for (let i = 0; i < child.length; i++) {
+        // console.log(child.item(i).nodeName);
+        test( child.item(i), dest, indent++ );
+    }
+}
 
 function explorer(){
     var new_div = document.createElement("div"); // <div> </div>
-    var text = document.createTextNode("allo"); // "allo"
-    new_div.appendChild(text); // <div> allo </div>
     document.body.appendChild(new_div); // rajoute div comme étant un enfant de body
-    test(document);
+    test(document, new_div, 0);
 }
-
-
-
-
-// LA POUBELLE
 
 
