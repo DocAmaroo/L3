546025f3ffa9b61665bf5d87f092cb531742b78c
C.Thomas <c.tom34270@gmail.com>
2020-05-17 14:08:19 +0200

    nettoyage S5/HLIN505

---
+++ S5/HLIN505_java/TP/CORRECTION_PROF/TD1HLIN505_2018/surchargeEtRedefinition/surchargeEtRedefinition/Document.java
@@ -0,0 +1,21 @@
+package surchargeEtRedefinition;
+
+public class Document {
+	private String titre;
+
+	public String getNom() {
+		return titre;
+	}
+
+	public Document(String titre) {
+		super();
+		this.titre = titre;
+	}
+
+	@Override
+	public String toString() {
+		return "Document [titre=" + titre + "]";
+	}
+
+}
+
